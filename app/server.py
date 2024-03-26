a0309067441667c80c9c948db7e83479826c8a0b
Sven Duve <svenduve@gmail.com>
2024-03-26 15:42:29 +0100

    added new chains

--- app/server.py
+++ app/server.py
@@ -132,6 +132,7 @@ def _format_chat_history(chat_history: List[Tuple]) -> str:
 
 # Get context from the website:
 # loader = WebBaseLoader(["https://www.express-kabel.de/ueber-uns/"])
+print(os.listdir("./context"))
 loader = TextLoader("./context/ek_context.md")
 data = loader.load()
 
